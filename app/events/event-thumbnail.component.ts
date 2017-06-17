f0261ff2b6cb74587d45400470c6adfe6d493971
mduberstein <mduberstein@gmail.fake>
2017-06-17 18:00:33 -0400

    Module 3, Communicating with Parent Component - before click button and handlers

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core'
+import {Component, Input, Output, EventEmitter} from '@angular/core'
 
 @Component ({
     selector: 'event-thumbnail',
@@ -13,10 +13,19 @@ import {Component, Input} from '@angular/core'
                 <span>&nbsp;</span>
                 <span>{{event.location.city}}, {{event.location.country}}</span>
             </div>
+            <div>
+                <button class="btn btn-primary" (click)="handleClickMe()">Click me!</button>
+            </div>
         </div>
         `
 })
 
 export class EventThumbnailComponent{
     @Input() event:any
+    @Output() eventClick = new EventEmitter()
+    handleClickMe(){
+        console.log('clicked in child!')
+        this.eventClick.emit('foo')
+        this.eventClick.emit(this.event.name)
+    }
 }
