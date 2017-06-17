d427fd4933d74a7ca750424c426b02d70d43a2d7
mduberstein <mduberstein@gmail.fake>
2017-06-17 18:03:23 -0400

    Module 3, Communicating with Parent Component. End of clip. Click bindings and event handlers removed

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -13,19 +13,10 @@ import {Component, Input, Output, EventEmitter} from '@angular/core'
                 <span>&nbsp;</span>
                 <span>{{event.location.city}}, {{event.location.country}}</span>
             </div>
-            <div>
-                <button class="btn btn-primary" (click)="handleClickMe()">Click me!</button>
-            </div>
         </div>
         `
 })
 
 export class EventThumbnailComponent{
     @Input() event:any
-    @Output() eventClick = new EventEmitter()
-    handleClickMe(){
-        console.log('clicked in child!')
-        this.eventClick.emit('foo')
-        this.eventClick.emit(this.event.name)
-    }
 }
--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -6,7 +6,7 @@ import {Component} from '@angular/core'
         <div>
         <h1>Upcoming Angular 2 Events</h1>
         <hr/>
-        <event-thumbnail (eventClick)="handleEventClicked($event)" [event]="event1"></event-thumbnail>
+        <event-thumbnail [event]="event1"></event-thumbnail>
         `
 })
 export class EventsListComponent{
@@ -23,9 +23,5 @@ export class EventsListComponent{
             city: 'London',
             country: 'England'
         }
-    }
-
-    handleEventClicked(data){
-        console.log('received:', data);
     }
 }
