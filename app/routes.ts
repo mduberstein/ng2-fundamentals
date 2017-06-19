d2a756706a5cb12e56af70c75d8954a615eb9332
mduberstein <mduberstein@gmail.fake>
2017-06-19 14:21:23 -0400

    Module 5. Routing and Navigating Pages. Clip: Adding your First Route. Final clip state

--- app/routes.ts
+++ app/routes.ts
@@ -4,6 +4,6 @@ import {EventDetailsComponent} from './events/event-details/event-details.compon
 
 export const appRoutes = [
     {path: 'events', component: EventsListComponent},
-    {path: 'event/:id', component: EventDetailsComponent},
+    {path: 'events/:id', component: EventDetailsComponent},
     {path: '', redirectTo: '/events', pathMatch:'full'},
 ]
