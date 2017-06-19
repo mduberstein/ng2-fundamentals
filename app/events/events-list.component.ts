035fe8d3106e26be9045ed0d69d9d84efbe9a766
mduberstein <mduberstein@gmail.fake>
2017-06-19 09:57:44 -0400

    Module 4. Clip2: Intergrating toastr by wrapping it into a service

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core'
 import {EventService} from './shared/event.service'
-
+import {ToastrService} from '../common/toastr.service'
 @Component({
     selector: 'events-list',
     template: `
@@ -18,13 +18,13 @@ import {EventService} from './shared/event.service'
 
 export class EventsListComponent implements OnInit {
     events:any[]
-    constructor (private eventService: EventService){
+    constructor (private eventService: EventService, private toastr: ToastrService){
     }
     ngOnInit(){
         this.events = this.eventService.getEvents();
     }
 
     handleThumbnailClick(eventName){
-        toastr.success(eventName)
+        this.toastr.success(eventName)
     }
 }
