5d6e0413a2add97facb93436153f1f8f7c8f2429
mduberstein <mduberstein@gmail.fake>
2017-06-19 13:43:00 -0400

    Module 5. Routing and Navigating Pages. Clip: Adding your First Route

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -2,7 +2,6 @@ import {Component, OnInit} from '@angular/core'
 import {EventService} from './shared/event.service'
 import {ToastrService} from '../common/toastr.service'
 @Component({
-    selector: 'events-list',
     template: `
         <div>
             <h1>Upcoming Angular 2 Events</h1>
