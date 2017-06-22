a32ccd8693c81946ac07cdf43c221d262b15c42f
mduberstein <mduberstein@gmail.fake>
2017-06-21 20:31:38 -0400

    Module 5. Clip: Lazily Loading Feature Modules. Added missed user folder files

---
+++ app/user/user.routes.ts
@@ -0,0 +1,6 @@
+import {ProfileComponent} from './profile.component'
+
+export const userRoutes = [
+    //actual Url would be /user/profile, see route.ts
+    {path: 'profile', component: ProfileComponent}
+]
