a348c7357d45d18ff0e79abe8d3f4245bb2e4103
mduberstein <mduberstein@gmail.fake>
2017-06-21 20:31:07 -0400

    Module 5. Clip: Lazily Loading Feature Modules.

--- app/nav/navbar.component.ts
+++ app/nav/navbar.component.ts
@@ -7,6 +7,7 @@ import {Component} from '@angular/core'
         .nav.navbar-nav {font: 15px;}
         #searchForm {margin-right: 100px;}
         @media (max-width: 1200px) {#searchForm {display:none}}
+        li > a.active {color: #F97924}
     `]
 })
 
