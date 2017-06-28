0a9b08c0e49072e0b57daf8a7223f32f3d6c8d2d
mduberstein <mduberstein@gmail.fake>
2017-06-27 20:40:01 -0400

    Module 6. Clip: Creating cusom validators, before making it reusable

--- app/events/event-details/create-session.component.ts
+++ app/events/event-details/create-session.component.ts
@@ -31,7 +31,7 @@ export class CreateSessionComponent{
         this.presenter = new FormControl('', Validators.required);
         this.duration = new FormControl('', Validators.required);
         this.level = new FormControl('', Validators.required);
-        this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400), this.restrictedWords]);
+        this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400), this.restrictedWords(['foo', 'bar'])])
 
         this.newSessionForm = new FormGroup({
             name: this.name,
@@ -42,11 +42,21 @@ export class CreateSessionComponent{
         })
     }
     //validator function
-    private restrictedWords(control: FormControl):{[key: string]: any}
-    {   //return error object contains a key matching the validator name
-        return control.value.includes('foo')?{'restrictedWords':'foo'}:null
+    // private restrictedWords(control: FormControl):{[key: string]: any}
+    // {   //return error object contains a key matching the validator name
+    //     return control.value.includes('foo')?{'restrictedWords':'foo'}:null
+    // }
+    private restrictedWords(words: string []){
+        return (control: FormControl):{[key:string]:any}=>{
+            //validator passed
+            if(!words){
+                return null
+            }
+            let invalidWords = words.map(w=>control.value.includes(w) ? w : null).filter(w=>w != null)
+            return invalidWords && invalidWords.length > 0 ?{'restrictedWords': invalidWords.join(', ')}: null
+        }
     }
-    
+
     saveSession(formValues){
         //console.log(formValues);
         debugger;
