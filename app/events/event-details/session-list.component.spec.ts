9ffd9a97f72a0faf1361ab531afe60cac2828474
mduberstein <mduberstein@gmail.fake>
2017-08-23 18:36:04 -0400

    Module 15: after Clip "Testing components with Isolated Tests". End of Module

---
+++ app/events/event-details/session-list.component.spec.ts
@@ -0,0 +1,48 @@
+import { SessionListComponent } from './session-list.component'
+import { ISession } from '../shared/event.model'
+
+describe('SessionListComponent', () => {
+    let component: SessionListComponent;
+    let mockAuthService, mockVoterService;
+    beforeEach(() => {
+        component = new SessionListComponent(mockAuthService, mockVoterService);
+    })
+
+    describe('ngOnChanges', () => {
+        it('should filter the sessions correctly', () => {
+            //Arrange
+            component.sessions = <ISession[]>[
+                { name: 'session 1', level: 'intermediate' },
+                { name: 'session 2', level: 'intermediate' },
+                { name: 'session 3', level: 'beginner' }
+            ];
+            component.filterBy = 'intermediate';
+            component.sortBy = 'name';
+            component.eventId = 3;
+            //in production this event happens automatically, in the test we have to call it
+            //Act
+            component.ngOnChanges();
+            //Assert
+            expect(component.visibleSessions.length).toBe(2);
+        })
+
+        it('should sort the sessions correctly', () => {
+            //Arrange
+            component.sessions = <ISession[]>[
+                { name: 'session 1', level: 'intermediate' },
+                { name: 'session 3', level: 'intermediate' },
+                { name: 'session 2', level: 'beginner' }
+            ];
+            component.filterBy = 'all';
+            component.sortBy = 'name';
+            component.eventId = 3;
+            //in production this event happens automatically, in the test we have to call it
+            //Act
+            component.ngOnChanges();
+            //Assert
+            expect(component.visibleSessions[2].name).toBe('session 3');
+        })
+    })
+})
+
+
--- app/test.spec.ts
+++ app/test.spec.ts
@@ -1,9 +1,12 @@
-describe('first tests', ()=>{
-    it('should be true', ()=> {
-        expect(true).toBe(true);
-    })
+//these are the tests to check the functionality of karma and jasmine installation
+//they are commented after some real tests have been writtent
 
-    it('should be false', ()=>{
-        expect(false).toBe(false);
-    })
-})
+// describe('first tests', ()=>{
+//     it('should be true', ()=> {
+//         expect(true).toBe(true);
+//     })
+
+//     it('should be false', ()=>{
+//         expect(false).toBe(false);
+//     })
+// })
